89f84cf72f0e50016ce2cdcd6a04115689f1eecf
Mayur505x <mayurpawar8691@gmail.com>
2022-05-11 10:50:12 +0530

    Added: Calculate fare program

--- test/TestFare.java
+++ test/TestFare.java
@@ -12,13 +12,12 @@ public class TestFare {
         double totalFare = rideFare.calculateFare(10,2);
         Assert.assertEquals(102, totalFare);
     }
+
     @Test
     public void givenDistanceAndTimeWhenCalculatedShouldReturnMinimumRideFare() {
         FirstRide minimumRideFare = new FirstRide();
         double totalFare = minimumRideFare.calculateFare(0.2, 2);
         Assert.assertEquals(5,totalFare);
-    }
-
-
-
+    
+	}
 }
